fdd1d9af8fca0a7ecf2b07800ddad61a88414219
ojw <ojoonwoo@gmail.com>
2018-10-02 17:51:21 +0900

    모바일 코딩 완료

---
+++ m/popup.php
@@ -0,0 +1,84 @@
+<div class="popup pt-result" id="pt-result">
+	<button type="button" class="popup-close" data-popup="@close"></button>
+	<div class="inner">
+		<div class="title-area">
+			<span class="title">시카 A/S 신청서</span>
+		</div>
+		<div class="line">
+		</div>
+		<div class="apply">
+			<div class="box ap_name">
+				<div class="label">
+					<span>신청인</span>
+				</div>
+				<div class="rs_req">
+					<span>김더마</span>
+				</div>
+			</div>
+			<div class="box ap_goods">
+				<div class="label">
+					<span>신청 제품</span>
+				</div>
+				<div class="rs_req">
+					<span>이OOOO</span>
+				</div>
+			</div>
+			<div class="box ap_req">
+				<div class="label">
+					<span>요구 사항</span>
+				</div>
+				<div class="rs_req">
+					<span>진정력</span>
+				</div>
+			</div>
+		</div>
+		<div class="result-text">
+			<div class="result-image">
+				<img src="./images/popup_result1.png" alt="" class="_1">
+			</div>
+		</div>
+		<div class="btn-area">
+			<button type="button">신청 완료</button>
+			<p>A/S는 1가구당 1회에 한해 무상 진행됩니다</p>
+		</div>
+	</div>
+</div>		
+<div class="popup pt-agree" id="pt-agree1">
+	<button type="button" class="popup-close" data-popup="@close"></button>
+	<div class="inner">
+		<div class="title-area">
+			<span>개인 정보 수집 및 이용에 관한 동의</span>
+		</div>
+		<div class="line"></div>
+		<div class="desc">
+			<div class="desc-inner">
+				<p style="margin-bottom: 5px;">'나오스코리아 유한회사’(이하 ‘회사’는) 다음과 같이 귀하의 개인정보를 수집, 이용합니다.</p>
+				<p style="margin-bottom: 5px;">회사는 본 이벤트를 위하여 다음과 같이 고객님의 개인정보를 수집 및 이용합니다.</p>
+				<p class="items first">◎수집 항목 : 이름, 연락처, 주소</p>
+				<p class="items">◎이용 목적 : 이벤트 진행을 위한 본인 확인, 경품 제공과 정보 공지</p>
+				<p class="items">및 불만 처리 등을 위한 연락처 확보</p>
+				<p class="items" style="margin-bottom: 5px;">◎보유 기간 : 이벤트 종료 후 1년 이내 파기</p> 
+				<p class="small first">귀하는 개인정보보호법에 따라 개인정보 수집/이용 동의를 거부 하실 수 있으며, 거부 시에는 해당 이벤트에 응모할 수 없음을 알려드립니다.</p>
+				<p class="small">귀하가 제공한 개인정보는 당사의 개인정보보호정책에 따라 안전하게 관리되며, 본 이벤트 종료 후 귀하로부터 제공받은 개인정보는 별도 동의한 마케팅 활용목적과 용도 이외에 제 3자에게 제공되거나 활용되지 아니합니다.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="popup pt-agree" id="pt-agree2">
+	<button type="button" class="popup-close" data-popup="@close"></button>
+	<div class="inner">
+		<div class="title-area">
+			<span>개인 정보 취급 위탁에 대한 동의</span>
+		</div>
+		<div class="line"></div>
+		<div class="desc">
+			<div class="desc-inner">
+				<p style="margin-bottom: 5px;">당사는 원활한 행사진행을 위하여 아래의 전문업체에 업무를 위탁 처리하고 있습니다.</p>
+				<p class="items first">◎수탁자 : 미니버타이징(주)</p>
+				<p class="items" style="margin-bottom: 5px;">◎위탁하는 업무의 내용 : 이벤트 당첨자 확인, 경품발송, MMS발송</p>
+				<p class="small first">수탁업체가 제공받은 귀하의 개인정보는 당사의 개인정보보호정책에 따라 안전하게 관리됩니다.</p>
+			</div>
+		</div>
+	</div>
+</div>
