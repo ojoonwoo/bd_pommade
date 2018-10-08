7aaf550caf49a038c627bdd4d22e358267bf0ff5
ojoonwoo <ojoonwoo@gmail.com>
2018-10-08 18:35:51 +0900

    원복

--- head.php
+++ head.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<link rel="stylesheet" href="./css/reset.css">
-	<link rel="stylesheet" href="./css/style_back.css">
+	<link rel="stylesheet" href="./css/style.css">
     <script src="./js/jquery-1.11.2.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js"></script>
     <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script>
