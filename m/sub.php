07b40ed6eebc17590f2c825554fd691b68566f92
kyhfan <kyhfan@gmail.com>
2018-10-11 12:10:43 +0900

    이벤트 안내 문구 수정

--- m/sub.php
+++ m/sub.php
@@ -179,7 +179,7 @@
 								A/S 신청 접수
 							</button>
 							<div class="extra-notice">
-								<span>A/S는 포마드 샘플 소진 시까지만 진행됩니다</span>
+								<span>A/S 샘플 키트는 1가구당 1회에 한해 무상 진행됩니다</span>
 							</div>
 						</div>
 					</div>
