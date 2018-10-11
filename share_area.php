827622ed285b7576fa674f15f5cd1e76b11e3b4d
kyhfan <kyhfan@gmail.com>
2018-10-11 11:40:34 +0900

    1111

--- share_area.php
+++ share_area.php
@@ -1,4 +1,7 @@
 					<div class="share-area">
+                        <a href="https://www.instagram.com/bioderma_kr/" target="_blank">
+						    <button class="insta"></button>
+                        </a>
 						<button class="fb" onclick="sns_share('fb', 'main')"></button>
 						<button class="kt" onclick="sns_share('kt', 'main')"></button>
 					</div>
